de43c47d2cb3ca82048e93c2aa6ba2d4e072ee85
SweetLanaMay <121062437+SweetLanaMay@users.noreply.github.com>
2024-01-17 16:45:13 +0200

    new styles

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
+import logoImg from '../../images/logo.png';
 
 const Searchbar = ({ onSubmit }) => {
   const [query, setQuery] = useState('');
@@ -16,6 +17,11 @@ const Searchbar = ({ onSubmit }) => {
 
   return (
     <header className={css.searchbar}>
+      <a href="/" className={css.logoContainer}>
+        <img src={logoImg} alt="ImageSearch Logo" className={css.logo} />
+        <h1 className={css.title}>ImageSearch</h1>
+      </a>
+      
       <form className={css.form} onSubmit={handleSubmit}>
         <button type="submit" className={css.searchButton}>
           <span className={css.buttonLabel}>Search</span>
